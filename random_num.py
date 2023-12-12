6b288f39b68a28b22a2379e60c1abb59781fc285
nurbilu <nurb111@gmail.com>
2023-12-12 13:01:20 +0200

    5th commit

--- random_num.py
+++ random_num.py
@@ -2,13 +2,16 @@
 print 5 random numbers 
 
 '''
-
+from icecream import ic 
 import random
 
 def rand_num():
-    random_numbers = [random.randint(1, 100) for _ in range(5)]
-    print("5 lottery Random Numbers:", random_numbers)
+    random_numbers = [random.randint(1, 41) for _ in range(5)]
+    ic("5 lottery Random Numbers:", random_numbers)
     
 
 if __name__ == "__main__": 
+    ic.disable()
+    rand_num()
+    ic.enable()
     rand_num()
