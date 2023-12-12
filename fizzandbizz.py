05d01e09a5d32d6cf26a1ccdab819246888f092a
nurbilu <nurb111@gmail.com>
2023-12-12 12:08:02 +0200

    3rd commit

--- fizzandbizz.py
+++ fizzandbizz.py
@@ -4,7 +4,7 @@ cretae fizz & bizz
 
 def fizz_bizz(n):
     for i in range(1, n + 1):
-        result = "FizzBizz" if i % 3 == 0 and i % 5 == 0 else "Fizz" if i % 3 == 0 else "Bizz" if i % 5 == 0 else i
+        result = "FizzBuzz" if i % 3 == 0 and i % 5 == 0 else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else i
         print(result)
 
 fizz_bizz(int(input("please enter a number")))
