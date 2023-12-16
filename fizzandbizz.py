eb2062e6db9a6a555bf28430a3553237004c3650
nurbilu <nurb111@gmail.com>
2023-12-16 17:40:00 +0200

    6th of commit

--- fizzandbizz.py
+++ fizzandbizz.py
@@ -1,10 +1,15 @@
 '''
 cretae fizz & bizz
 '''
-
+from icecream import ic 
 def fizz_bizz(n):
     for i in range(1, n + 1):
-        result = "FizzBuzz" if i % 3 == 0 and i % 5 == 0 else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else i
-        print(result)
+        result = "FizzBuzz" if i % 15 == 0 else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else i
+        ic(result)
 
-fizz_bizz(int(input("please enter a number")))
+
+if __name__ == "__main__":
+    ic.disable()
+    ic("hello world")
+    ic.enable()
+    fizz_bizz(int(input("please enter a number ")))
